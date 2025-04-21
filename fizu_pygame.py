d611e1e45a10aefa29b1959f311a28365b790926
Fizu <fizuto0@gmail.com>
2025-04-21 16:27:48 +0200

    000.02
    added delta time. movement for dynamic sprites, mouse positions

--- fizu_pygame.py
+++ fizu_pygame.py
@@ -1,6 +1,8 @@
 import pygame as pg
+from pygame.math import *
 import math
 import sys
+import time
 
 
 
@@ -14,29 +16,37 @@ Classes like timer, new sprites for easier work and points of center
 
                                                             
 To add:
-+ camera, with camera work. Stuck movement, mouse dependant movement, smooth movement
-+ global mouse position (include blit size and win size) and also local position
++ smooth camera
++ mouse dependant camera
 + rotation from point
 + image groups
 + new image class
-+ new function in sprite and image that tells if local mouse is in them
-DONE timer, but time is set in activate, they have func when they finish and/or 
++ timer, but time is set in activate, they have func when they finish and/or 
 can delete themself
-DONE timer groups
++ timer groups
 + easier text that is also an image
 + something like character node that checks for collision
 + something like enviroment node that has collision but doesnt check for them
++ class for creating worlds
+
++ whichever funcition shouldnt be used by user should have an underscore
 
 DONE but add more ||| add bliting and updating in handlers in loop, for all sprites and images in group
 DONE change rects to frects from community edition
 DONE new sprites have init position
 DONE add colorkey for sprites
 DONE new sprite class, with adding self to group, y sort, center point
+DONE local mouse position
+DONE clipped camera
+DONE global mouse position (include blit size and win size)
+DONE new function in sprite and image that tells if local mouse is in them
+(add local for hud elements)
 
 LATER:
-+ more complex blitting, all sprites are blitted, z and noz, but sorted good. 
-+ z sort function for sprites(not in class)
-+ sprite groups (all, world etc)
++ functions for sprites with input that can be changed
+animation and audio, also putting these functions in update untouchable
+
+
 
 ----------------------------------------------------------------------------
 """
@@ -55,8 +65,8 @@ Variables for setting everything
 #loop init variables
 win_size = (1280,720)
 blit_size = (960, 540)
-scale_w_to_b = (blit_size[0] / win_size[0], blit_size[1] / win_size[1])
-scale_b_to_w = (win_size[0] / blit_size[0] , win_size[1] / blit_size[1])
+scale_w_to_b = Vector2(blit_size[0] / win_size[0], blit_size[1] / win_size[1])
+scale_b_to_w = Vector2(win_size[0] / blit_size[0] , win_size[1] / blit_size[1])
 game_caption = "Game"
 #game_pic = 
 game_fps = 60
@@ -66,7 +76,9 @@ game_fps = 60
 Groups of sprites and images to blit, update or something
 """
 all_timers = []
-sprites_noz = pg.sprite.Group()
+all_sprites = pg.sprite.Group()
+all_static = pg.sprite.Group()
+all_dynamic = pg.sprite.Group()
 #layers
 #maybe colliders and collidable
 # player, enemies and npcs
@@ -74,9 +86,28 @@ sprites_noz = pg.sprite.Group()
 #
 
 
+
+
+
+# variables to access, but not to change
+camera_off = Vector2(0,0)
+camera_tar = Vector2(0,0)
+
+
+
+
 """
 Usable functions during work, mostly with returns
 """
+
+#getting mouse location on blit screen
+def mouse_local_position():
+    pos_x, pos_y = pg.mouse.get_pos()
+    pos = Vector2(pos_x * scale_w_to_b[0], pos_y * scale_w_to_b[1])
+    return pos
+
+def mouse_global_position():
+    return camera_off + mouse_local_position()
 
 #for clmaping values between two limits
 def clamp(value, min_v, max_v):
@@ -125,26 +156,138 @@ def split_spr_name(name):
 """
 Some classes during work. Like basic sprite or image, timer and text on screen
 """
-class NewSprite_noZ(pg.sprite.Sprite):
+class Sprite(pg.sprite.Sprite):
     def __init__(self, start_pos = (0,0)):
         super().__init__()
-        sprites_noz.add(self)
+        all_sprites.add(self)
         self.layer_blit = 0
         self.colorkey = (0,0,0)
         self.start_pos = start_pos
 
-        self.image = pg.Surface((10,10)) #size of collision
+        #collision
+        self.image = pg.Surface((20,20)) #size of collision
         self.image.set_colorkey(self.colorkey)
+        self.image.fill((0,0,255))
         self.rect = self.image.get_frect(topleft = self.start_pos) #colision
-    
+
+        #visual
+        self.sprite = pg.Surface((14,14))
+        self.sprite.fill((255,255,255))
+        self.v_off = Vector2(3,3)
+
+        #variables that always needed to be updated, like center of sprite for camera
+        self.center = self.rect.center
+
+
+        self.init()
+
+    def init(self):
+        pass
+
 
 #TODO later add something about animation
 
     def update(self):
         pass
 
+    def _update(self, dt):
+        self.center = self.rect.center
+        self.update()
 
-#static body
+    def remove(self):
+        all_sprites.remove(self)
+        if all_static.has(self):
+            all_static.remove(self)
+        if all_dynamic.has(self):
+            all_dynamic.remove(self)
+    
+    def mouse_in_rect(self):
+        if self.rect.collidepoint(mouse_global_position()):
+            return True
+        else:
+            return False
+
+
+#from this sprite add:
+"""
+STATIC BODY
+The one that doesnt collide with anything, but is collideable. Probably no movement in them
+Like tiles or something
+"""
+class StaticBody(Sprite):
+    def __init__(self, start_pos=(0, 0)):
+        super().__init__(start_pos)
+        all_static.add(self)
+
+
+
+"""
+DYNAMIC BODY
+A one that collides with static bodies.
+Like entities or something
+"""
+class DynamicBody(Sprite):
+    def __init__(self, start_pos=(0, 0)):
+        super().__init__(start_pos)
+        all_dynamic.add(self)
+
+        self.input_vec = Vector2(0,0)
+        self.move_vec = Vector2(0,0) #moving the sprite
+
+    #filled by user
+    def x_axis_movement(self):
+        self.move_vec.x -= 1
+
+
+    def _x_axis_collision(self):
+        for static in all_static.sprites():
+            if self.rect.colliderect(static.rect):
+                #right
+                if self.move_vec.x >0: 
+                    self.rect.right = static.rect.left
+                #left
+                elif self.move_vec.x <0:
+                    self.rect.left = static.rect.right
+                
+                self.move_vec.x = 0
+
+    def _movement_x_axis(self, dt):
+        self.rect.centerx += self.move_vec.x * dt
+
+
+    #filled by user
+    def y_axis_movement(self):
+        pass
+    def _y_axis_collision(self):
+        for static in all_static:
+            if self.rect.colliderect(static.rect):
+                #bottom
+                if self.move_vec.y >0:
+                    self.rect.bottom = static.rect.top
+                #up
+                elif self.move_vec.y <0:
+                    self.rect.top = static.rect.bottom
+                self.move_vec.y = 0
+
+    def _movement_y_axis(self, dt):
+        self.rect.centery += self.move_vec.y * dt
+    
+    def _update(self, dt):
+        self.center = self.rect.center
+        self.update()
+
+        self.x_axis_movement()
+        self._movement_x_axis(dt)
+        self._x_axis_collision()
+        self.y_axis_movement()
+        self._movement_y_axis(dt)
+        self._y_axis_collision()
+
+
+
+
+
+
 
 #Timer 
 class Timer():
@@ -182,8 +325,34 @@ class Timer():
 
     
 
+class Camera():
+    def __init__(self):
+        self.camera_off = Vector2(0,0)
+        self.max_mousedis = 0.4
+        self.x = 0
+        self.y = 0
 
+    def clipped_movement(self):
+        global camera_tar
+        self.x, self.y = camera_tar
 
+        self.x = int(self.x - blit_size[0]/2)
+        self.y = int(self.y - blit_size[1]/2)
+        self.camera_off = Vector2(self.x, self.y)
+        global camera_off
+        camera_off = self.camera_off
+
+    #smooth TODO
+
+    #mouse based TODO
+    
+    def display(self, blit_screen):
+        l_sprites = all_sprites.sprites()
+        l_sprites.sort(key = lambda layer: layer.layer_blit)
+        for sprite in l_sprites:
+            blit_screen.blit(sprite.image, sprite.rect.topleft - camera_off)
+            blit_screen.blit(\
+                sprite.sprite, sprite.rect.topleft + sprite.v_off - camera_off)
 
 """
 Main class with a game loop inside
@@ -197,14 +366,16 @@ class GameLoop():
         pg.display.set_caption(game_caption)
         self.clock = pg.time.Clock()
 
-        self.player = NewSprite_noZ((100,100))
-        self.player.layer_blit = 1
-        self.player.image.fill((0,255,0))
-        self.plad = NewSprite_noZ((105,105))
-        self.plad.image.fill((0,0,255))
+        self.next_time = time.time()
+        self.dt = 0
 
+        self.camera = Camera()
+        self.camera_tar = Vector2(0,0)
 
         self.init()
+
+
+
 
     def init(self):
         pass
@@ -214,10 +385,10 @@ class GameLoop():
     
 
     def update(self):
-        self.update_sprites_noz()
+        pass
 
     def display(self):
-        self.display_sprites_noz()
+        pass
 
 #once set functions that shouldnt be modified
     def input_handler(self):
@@ -227,16 +398,21 @@ class GameLoop():
                 pg.quit()
                 sys.exit()
 
-    def update_handler(self):
+    def update_handler(self, dt):
         for timer in all_timers:
             timer.update()
+        self.update_sprites(dt)
+
         self.update()
 
 
     def display_handler(self):
         self.screen.fill((0,0,0))
         self.blit_screen.fill((0,0,0))
+        self.camera.display(self.blit_screen)
+
         self.display()
+
         self.screen.blit(pg.transform.scale(self.blit_screen, win_size), (0,0))
 
 
@@ -244,20 +420,16 @@ class GameLoop():
 #shouldnt be modified by user
     def loop(self):
         while True:
+            self.dt = time.time() - self.next_time
+            self.next_time = time.time()
+
             self.input_handler()
-            self.update_handler()
+            self.update_handler(self.dt)
             self.display_handler()
 
             pg.display.flip()
             self.clock.tick(game_fps)
-
-    #put this in display()
-    def display_sprites_noz(self):
-        spritesNoZ = sprites_noz.sprites()
-        spritesNoZ.sort(key = lambda layer: layer.layer_blit)
-        for sprite in spritesNoZ:
-            self.blit_screen.blit(sprite.image, sprite.rect.topleft)
     
-    def update_sprites_noz(self):
-        for sprite in sprites_noz:
-            sprite.update()
+    def update_sprites(self, dt):
+        for sprite in all_sprites:
+            sprite._update(dt)
--- test.py
+++ test.py
@@ -1,9 +1,32 @@
 import fizu_pygame as fpg
 
-print(fpg.scale_b_to_w)
-print(fpg.scale_w_to_b)
-loop = fpg.GameLoop()
+
+
+#setting win sizes etc
+
+
+class Loop(fpg.GameLoop):
+    def __init__(self):
+        super().__init__()
+
+    def init(self):
+        self.player = fpg.DynamicBody((100,18))
+        self.enemy = fpg.StaticBody((0,0))
+        self.enemy.sprite.fill((0,0,0))
+        
+
+    def input(self):
+        pass
+    
+
+    def update(self):
+        self.camera.clipped_movement()
+
+    def display(self):
+        pass
 
 
 
+
+loop = Loop()
 loop.loop()
